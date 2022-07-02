adbc1ff5f68bcd9439346d7ec1f8eea737e1f4f8
Toma <tomasajtofficial@gmail.com>
2022-07-02 19:07:13 +0200

    add prompt at the beginning

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { config as dotenv } from 'dotenv'
+import prompt from 'prompt'
 
 type Pack = {
     id: number,
@@ -13,12 +14,49 @@ const sleep = (time: number) => new Promise(res => setTimeout(res, time))
 async function main() {
     const SESSION_ID = process.env.SESSION_ID
     if (!SESSION_ID) throw 'You need to set your SESSION_ID in the .env file.'
+    prompt.start({ message: "osu-packfetch" })
+    const pres = await prompt.get({
+        properties: {
+            pageFrom: {
+                description: 'From page',
+                type: 'number',
+                minimum: 1,
+                default: 1,
+                required: true
+            },
+            pageTo: {
+                description: 'To page',
+                type: 'number',
+                default: 83,
+                required: true,
+                conform: val => parseInt(val) >= parseInt(prompt.history('pageFrom')!.value)
+            },
+            packRegex: {
+                description: 'Pack regex',
+                type: 'string',
+                default: 'Taiko',
+                required: true,
+                conform: val => {
+                    try {
+                        new RegExp(val)
+                        return true
+                    }
+                    catch {
+                        return false;
+                    }
+                }
+            }
+        }
+    })
+    const from = parseInt(pres.pageFrom.toString())
+    const to = parseInt(pres.pageTo.toString())
+    const packRegex = new RegExp(pres.packRegex.toString())
     let str = ""
-    for (let i = 1; i <= 83; i++) {
+    for (let i = from; i <= to; i++) {
         console.log(i);
         const packs = await getPacksForPage(i)
         for (const pack of packs) {
-            if (!pack.name.includes("Taiko")) continue;
+            if (!packRegex.test(pack.name)) continue;
             console.log(pack.name)
             str += await getLinkForPackId(pack.id, SESSION_ID) + '\n'
             await sleep(1000)
